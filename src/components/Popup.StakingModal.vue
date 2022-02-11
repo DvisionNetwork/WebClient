4441bd368ca71d38f708520bfe1caf012f11f35d
can.pham <can.pham@sotatek.com>
2022-02-11 11:07:39 +0700

    update

--- src/components/Popup.StakingModal.vue
+++ src/components/Popup.StakingModal.vue
@@ -351,11 +351,9 @@ export default {
 
 			params = JSON.parse(JSON.stringify(params))
 
-			console.log('params', params)
-
 			await contractConn.methods
 				.deposit(
-					this.getUint8, // 30 days = 1, 60 days = 2, 90 days = 3
+					this.getUint8(), // 30 days = 1, 60 days = 2, 90 days = 3
 					params
 				)
 				.send({
